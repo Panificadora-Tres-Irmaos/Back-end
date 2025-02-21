0996eb43a8620b03a49c2e43fddf547aed23f11c
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-02-21 08:44:11 -0300

    First commit

---
+++ src/main/java/com/example/Backend/_3/Irmaos/Backend3IrmaosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend._3.Irmaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend3IrmaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend3IrmaosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Backend/_3/Irmaos/Backend3IrmaosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend._3.Irmaos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend3IrmaosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
