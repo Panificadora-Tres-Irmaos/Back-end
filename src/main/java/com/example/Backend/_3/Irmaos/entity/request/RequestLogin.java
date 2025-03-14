9353dad4f6f39be67cb03cd187fd1991a0ac6866
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-03-14 07:16:05 -0300

    Adicionando uma classe de Request para os itens de login

---
+++ src/main/java/com/example/Backend/_3/Irmaos/entity/request/RequestLogin.java
@@ -0,0 +1,29 @@
+package com.example.Backend._3.Irmaos.entity.request;
+
+public class RequestLogin {
+
+    private String email;
+
+    private String senha;
+
+    public RequestLogin(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
